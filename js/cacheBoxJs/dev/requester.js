9e0d4a4d39f3a67aa1083e6f0b9cfe4346abea31
Yury A Troynov <icokie@yandex.ru>
2017-04-16 21:05:36 +0500

    cacheBoxJS -- requester

---
+++ js/cacheBoxJs/dev/requester.js
@@ -0,0 +1,33 @@
+'use strict';
+
+export default class Requester {
+
+    /**
+     * @description get response from url
+     * @param url
+     * @param async
+     * @returns {Promise}
+     */
+
+    get(url, async) {
+
+        return new Promise((resolve, reject) => {
+
+            let request = new XMLHttpRequest();
+
+            request.open('get', url, async || true);
+            request.send();
+
+            request.onload = (response) => {
+                resolve(response.target);
+            };
+
+            request.onerror = (response) => {
+                reject(response);
+            };
+
+        });
+
+    }
+
+}
