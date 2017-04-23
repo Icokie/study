d44c0ca19f6d46ad8c2ab5895a9a4dd82315e2c3
Yury A Troynov <icokie@yandex.ru>
2017-04-23 21:10:15 +0500

    cacheBoxJS -- time stamps

--- js/cacheBoxJs/dev/cacheBox.js
+++ js/cacheBoxJs/dev/cacheBox.js
@@ -1,5 +1,5 @@
-import Requester from './requester';
-import Storage from './storage';
+import Requester from "./requester";
+import Storage from "./storage";
 
 export default  class CacheBox {
 
@@ -7,44 +7,89 @@ export default  class CacheBox {
 
         this.request = new Requester();
         this.storage = new Storage();
-        this.cache = 1;
+        this.time = new Date().getTime();
 
     }
 
-    dataLoad(url, cache) {
 
-        return new Promise((resolve, reject)=>{
+    set(key, data, options) {
+        return new Promise((resolve, reject) => {
 
-            let data = this.storage.pull(url);
+                // adding time
+                options.added = this.time;
 
+                // adding expiration time
+                if (options.hasOwnProperty('expireIn')) {
+                    options.expired = this.time + options.expireIn;
+                }
 
-            if(cache || this.cache && data) {
-
+                this.storage.push(key, data, options);
                 resolve(data);
+
+        });
+    }
+
+    get(url, cache) {
+        return new Promise((resolve, reject)=> {
+
+            if (cache) {
+
+                let data = this.storage.pull(url);
+
+                if (data) {
+
+                    resolve({data:data, status:1});
+
+                } else {
+
+                    this.request.get(url).then((data)=> {
+                        resolve({data:data.response, status:0});
+                    });
+
+                }
+
 
             } else {
 
                 this.request.get(url).then((data)=> {
-
-                    this.storage.push(url, data.response);
-
-                    resolve(data.response);
-
+                    resolve({data:data.response, status:0});
                 });
 
             }
 
         });
+    }
+
+    load(url, cache, options) {
+
+        return new Promise((resolve, reject)=> {
+
+            this.get(url, cache).then((response)=>{
+
+                if(1 == response.status) {
+
+                    resolve(response.data);
+
+                } else {
+
+                    resolve(response.data);
+                    this.set(url, response.data, options);
+
+                }
+
+            });
+
+        });
 
     }
 
-    dataExec(data) {
+    exec(data) {
 
         return new Promise((resolve, reject) => {
 
             let src;
 
-            if('string' == typeof data) {
+            if ('string' == typeof data) {
 
                 src = new Function(data);
                 src();
